a2eb4c82db698391deb506c8140b3fbd638c5194
Patrick Oliveira <oliveira.patrick94@gmail.com>
2016-09-05 21:14:20 -0300

    Ajuste da tela de ajuda - cadastro de sondagem modelo

--- app/src/main/java/com/project/sondagemocr/AjudaSondagemActivity.java
+++ app/src/main/java/com/project/sondagemocr/AjudaSondagemActivity.java
@@ -2,20 +2,21 @@ package com.project.sondagemocr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.*;
 
 public class AjudaSondagemActivity extends AppCompatActivity implements View.OnClickListener{
 
-    ImageButton imgVolta;
+    FloatingActionButton imgVolta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajuda_sondagem);
 
-        imgVolta = (ImageButton) findViewById(R.id.imgVolta);
+        imgVolta = (FloatingActionButton) findViewById(R.id.imgVolta);
 
         imgVolta.setOnClickListener(this);
     }
